ebe6299e091aeec89cfb09499d3717d57ce24b1c
Abhishek <abhitiku2003@gmail.com>
2024-02-07 14:05:17 +0530

    wrote aggregation pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -267,4 +267,71 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, user, "Image Updated Successfully"))
 })
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params
+
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is Missing")
+    }
+
+    const channel = await User.aggregate([{
+        $match: {
+            username: username?.toLowerCase()
+        }
+    },
+    {
+        $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscribers"
+        }
+    },
+    {
+        $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "subscriber",
+            as: "subscribedTo"
+        }
+    },
+    {
+        $addFields: {
+            subscribersCount: {
+                $size: "$subscribers"
+            },
+            channelsSubscribedToCount: {
+                $size: "$subscribedTo"
+            },
+            isSubscribed: {
+                $cond: {
+                    if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                    then: true,
+                    else: false,j
+                }
+            }
+        }
+    },
+    {
+        $project: {
+            fullName: 1,
+            username: 1,
+            subscribersCount: 1,
+            channelsSubscribedToCount: 1,
+            isSubscribed: 1,
+            avatar: 1,
+            coverImage: 1,
+            email: 1,
+        }
+    }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel Doesnt Exist")
+    }
+
+    return res.status(200).json(new ApiResponse(200, channel[0], "User Channel fetched Successfully"))
+})
+
 export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage }
